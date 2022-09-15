483521cdf0c85a415b9f653dcf512e979ba9abcc
Olami1998 <98036456+Olami1998@users.noreply.github.com>
2022-09-15 16:19:20 +0100

    hiit leki and western hotel
    Leki hotels and western hotel

---
+++ assets/js/menu.js
@@ -0,0 +1,24 @@
+let x = document.getElementById("k_items")
+
+let t1='<div class="k_item">'
+let t2='<div class="k_item_title">Spaggy mix </div>'
+let t3='<img src="assets/images/f1.jpg">'
+let t4='<button > Order this item</button>'
+let t5='</div>'
+
+let t6='<div class="k_item">'
+let t7='<div class="k_item_title">African swallow </div>'
+let t8='<img src="assets/images/f2.jpg">'
+let t9='<button > Order this item</button>'
+let t10='</div>'
+
+let t11='<div class="k_item">'
+let t12='<div class="k_item_title">Rice and Spag </div>'
+let t13='<img src="assets/images/f3.jpg">'
+let t14='<button > Order this item</button>'
+let t15='</div>'
+
+
+let data=t1+t2+t3+t4+t5+t6+t7+t8+t9+t10+t11+t12+t13+t14+t15
+
+x.innerHTML=data
